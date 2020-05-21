851b5120acab9f06dd19982845e366c784305465
Milagros <milagros_jesus98@hotmail.com>
2020-05-20 23:05:13 -0500

    CONEXION - PRUEBA

---
+++ MesonURP/CTR/Ctr_Recurso.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DAO;
+using DTO;
+
+namespace CTR
+{
+    public class Ctr_Recurso
+    {
+        Dao_Recurso dao_rec;
+        public Ctr_Recurso()
+        {
+            dao_rec = new Dao_Recurso();
+        }
+        public void Ctr_Registrar_Recurso(Dto_Recurso dto_Recurso)
+        {
+            dao_rec.Dao_Registrar_Recurso(dto_Recurso);
+        }
+    }
+}
