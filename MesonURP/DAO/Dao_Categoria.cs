61777711a13b8f76e68b09baf3fda89212e970b0
darian <dariangv@hotmail.com>
2020-06-03 17:00:32 -0500

    Creando  Dao_Categoria

--- MesonURP/DAO/Dao_Categoria.cs
+++ MesonURP/DAO/Dao_Categoria.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using DTO;
 
 namespace DAO
 {
     public class Dao_Categoria
     {
-       
+        SqlConnection conexion;
         public Dao_Categoria()
         {
+
+            conexion = new SqlConnection(ConexionBD.CadenaConexion);
+
+        }
+        public void Ingresar_Categoria(Dto_Categoria dto_cat)
+        {
+
+            string Insert = "INSERT T_Categoria(C_idCategoria, C_NombreCategoria, C_Descripcion) VALUES('" + dto_cat.idCategoria + "', '" + dto_cat.NombreCategoria + "', '" + dto_cat.Descripcion + "')";
+            SqlCommand sqlCommand = new SqlCommand(Insert, conexion);
+            conexion.Open();
+            sqlCommand.ExecuteNonQuery();
+            conexion.Close();
+        }
+        public bool Seleccionar_Categoria(Dto_Categoria dto_cat)
+        {
+            string Select = "SELECT * FROM Libros WHERE  C_idCategoria= '" + dto_cat.idCategoria + "'";
+              
+            SqlCommand command = new SqlCommand(Select, conexion);
+
+            conexion.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            bool hayRegistros = reader.Read();
+            if (hayRegistros)
+            {
+                
+                dto_cat.idCategoria = (int)reader[0];
+                dto_cat.NombreCategoria = (string)reader[1];
+                dto_cat.Descripcion = (string)reader[2];
+
+            }
+           
+            // Tener en cuanto que no pueden haber categoria regisrada
+            conexion.Close();
+
+            return hayRegistros;
+
+
         }
 
 
---
+++ MesonURP/DTO/Dto_Categoria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    public class Dto_Categoria
+    {
+        int idcategoria;
+        string nomCategoria;
+        string descripcion;
+        public Dto_Categoria() : this(0, "", "") { }
+        public Dto_Categoria(int icategoria, string nomcategoria, string descripcion)
+        {
+            this.idCategoria = icategoria;
+            this.NombreCategoria = nomcategoria;
+            this.Descripcion = descripcion;
+        }
+
+        public int idCategoria { get => idcategoria; set => idcategoria = value; }
+        public string NombreCategoria { get => nomCategoria; set => nomCategoria = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+    }
+}
