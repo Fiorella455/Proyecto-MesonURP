1e9d2201de57832b8541a9df735bf63b81dcc28d
darian <dariangv@hotmail.com>
2020-06-03 17:52:09 -0500

    Creando DTO_Estado_OC y DTO_Proveedor

---
+++ MesonURP/DTO/DTO_Estado_OC.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    class DTO_Estado_OC
+    {
+        public int EOC_idEstadoOC { get; set; }
+        public int EOC_NombreEstadoOC { get; set; }
+
+    }
+}
---
+++ MesonURP/DTO/DTO_Proveedor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    class DTO_Proveedor
+    {
+        public int P_idProveedor { get; set; }
+        public int P_RazonSocial { get; set; }
+        public int P_NumeroDocumento { get; set; }
+        public string Direccion { get; set; }
+        public string NombreContacto { get; set; }
+        public string TelefonoContacto { get; set; }
+        public string CorreoContacto { get; set; }
+        public string EP_idEstadoProveedor { get; set; }
+
+
+    }   }
+   
+
