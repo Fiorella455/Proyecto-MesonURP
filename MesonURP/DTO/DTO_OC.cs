b2cf8bac446d505125c7d71418f9ce17ddb4365f
carloslau2709 <carloslau2709@hotmail.com>
2020-06-03 01:50:12 -0500

    DTO_Clases

---
+++ MesonURP/DTO/DTO_OC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DTO
+{
+    public class DTO_OC
+    {
+        public int OC_idOrdenCompra { get; set; }
+        public string OC_TipoComprobante { get; set; }
+        public string OC_NumeroComprobante { get; set; }
+        public double OC_TotalCompra { get; set; }
+        public DateTime OC_FechaEmision { get; set; }
+        public int P_idProveedor { get; set; }
+    }
+}
+
