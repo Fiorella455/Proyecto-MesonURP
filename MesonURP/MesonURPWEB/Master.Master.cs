85c2a0848c5edb2cc08f1c64337781fced65b0e8
Milagros <milagros_jesus98@hotmail.com>
2020-06-10 16:30:09 -0500

    Modificando master1

--- MesonURP/MesonURPWEB/Master.Master.cs
+++ MesonURP/MesonURPWEB/Master.Master.cs
@@ -11,7 +11,57 @@ namespace MesonURPWEB
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Login();
+        }
+        public void Login()
+        {
+            if (Session["Login"] != null)
+            {
+                liSolicitarServ.Visible = false;
+                li1.Visible = false;
+                li2.Visible = false;
+                li3.Visible = false;
+                li4.Visible = false;
+                li5.Visible = false;
+                li6.Visible = false;
+                li7.Visible = false;
+                li8.Visible = false;
 
+                string[] localarray = (string[])Session["Login"];
+                string perfil = localarray[1];
+                if (perfil == "Administrador")
+                {
+                    string nombre = localarray[0];
+                    li0.Visible = true;
+                    li1.Visible = true;
+                    li2.Visible = true;
+                    li3.Visible = true;
+                    li4.Visible = true;
+                    li5.Visible = true;
+                    li6.Visible = true;
+                    lblNombre.Text = Convert.ToString(Session["NombreUsuario"]);
+                    lblApellido.Text = Convert.ToString(Session["ApellidoUsuario"]);
+                }
+
+                else if (perfil == "Encargado de Compras")
+                {
+                    string nombre = localarray[0];
+
+                    li7.Visible = true;
+                    li8.Visible = true;
+                    lblNombre.Text = Convert.ToString(Session["NombreUsuario"]);
+                    lblApellido.Text = Convert.ToString(Session["ApellidoUsuario"]);
+                }
+
+                else
+                {
+                    Response.Redirect("Login.aspx");
+                }
+            }
+        }
+        protected void btnSalida_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Home.aspx");
         }
     }
 }
