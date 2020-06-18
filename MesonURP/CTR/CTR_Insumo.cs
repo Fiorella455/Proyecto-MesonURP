f367a6343efbdee870bb1cfbf424714d034ae2ac
Katya Vergaray Carbajal <katyavergaray@outlook.com>
2020-06-18 10:24:24 -0500

    Manejar Stock

---
+++ MesonURP/CTR/CTR_Insumo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using DAO;
+
+namespace CTR
+{
+    public class CTR_Insumo
+    {
+        DAO_Insumo objDAO;
+        public CTR_Insumo() {
+            objDAO = new DAO_Insumo();
+        }
+        public DataTable ListarInsumo() {
+            return objDAO.SelectInsumo();
+        }
+        public DataTable BuscarInsumo(string nombreInsumo)
+        {
+            return objDAO.SelectInsumos(nombreInsumo);
+        }
+        public DataTable ListarDashboard() {
+            return objDAO.SelectDashboard();
+        }
+    }
+}
---
+++ MesonURP/DAO/ConexionBD.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DAO
+{
+    public class ConexionBD
+    {
+        public static string CadenaConexion
+        {
+            get
+            {
+                //MILAGROS
+                //return "data source=DESKTOP-928V5LN\\SQLEXPRESS; initial catalog=DB_MesonURP; integrated security=SSPI;";
+
+                //GRECIA
+                //return "Data Source= DESKTOP-LLEV629;database=DB_MesonURP;integrated security=SSPI;";
+
+                //KATYA
+                return "data source=LAPTOP-VNIPF8CA; initial catalog=DB_MesonURP; integrated security=SSPI;";
+            }
+        }
+    }
+}
